2bedcd3ddc504ded69304af9f736262a02e8acaf
awinogradov <winogradovaa@gmail.com>
2018-03-29 01:13:15 +0300

    chore: rewrite PluginBemJSD to TS

---
+++ packages/plugin-bemjsd/src/index.ts
@@ -0,0 +1,29 @@
+/// <reference types="@types/agrarium" />
+
+import { Plugin } from '@agrarium/plugin';
+import * as bemJsd from 'bem-jsd';
+
+export class PluginBemJSD extends Plugin {
+    async gather(component: Agrarium.IComponent) {
+        const bemjsd: {
+            jsd: any;
+            file: Agrarium.IBemEntity
+        }[] = [];
+
+        await this.walkSources({
+            files: component.files,
+            tech: 'js',
+        }, (result: Agrarium.IWalkSourcesResult) => {
+            let jsd = bemJsd(result.source);
+
+            // 'bemjsd' returns { jsdocType: 'root' } for files without JSDoc
+            if (Object.keys(jsd).length < 2) {
+                jsd = {};
+            }
+
+            bemjsd.push({ jsd, file: result.file });
+        });
+
+        return { bemjsd };
+    }
+}
