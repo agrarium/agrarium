f8567b2f8a97997447b92d97b04ceacce4f0643e
awinogradov <winogradovaa@gmail.com>
2018-03-29 01:13:15 +0300

    chore: rewrite PluginBemJSD to TS

--- packages/plugin-bemjsd/test/plugin-bemjsd.spec.js
+++ packages/plugin-bemjsd/test/plugin-bemjsd.spec.js
@@ -1,82 +1,31 @@
-const { getAgrarium } = require('@agrarium/test-helpers');
-const path = require('path');
 const { describe, it } = require('mocha');
-const chai = { expect } = require('chai');
+const chai = require('chai');
 const chaiSubset = require('chai-subset');
+
+const { agrarium } = require('@agrarium/test-helpers');
+const { PluginBemJSD } = require('../src');
 
 chai.use(chaiSubset);
 
-const PluginBemJSD = require('../plugin-bemjsd');
+const { expect } = chai;
 
 describe('Agrarium: Plugin BEM JSD', () => {
 
-    it('should return jsdoc as json', async () => {
-        expect(await getAgrarium({
-            src: [path.resolve(__dirname, 'test-data/blocks')],
+    it('returns jsdoc as json', async () => {
+        expect(await agrarium({
+            src: ['test/fixtures/blocks'],
             plugins: [new PluginBemJSD()]
         })).to.containSubset([
             {
-                bemjsd: [{
-                    jsd: {
-                        "jsdocType": "root",
-                        "modules": [
-                            {
-                                "jsdocType": "module",
-                                "name": "button",
-                                "exports": {
-                                    "jsdocType": "class",
+                data: {
+                    bemjsd: [{
+                        jsd: {
+                            "jsdocType": "root",
+                            "modules": [
+                                {
+                                    "jsdocType": "module",
                                     "name": "button",
-                                    "proto": {
-                                        "jsdocType": "type",
-                                        "jsType": "Object",
-                                        "props": [
-                                            {
-                                                "key": "getText",
-                                                "val": {
-                                                    "jsdocType": "type",
-                                                    "jsType": "Function",
-                                                    "returns": {
-                                                        "jsdocType": "returns",
-                                                        "description": "",
-                                                        "jsType": "String"
-                                                    },
-                                                    "description": "Returns text of the button"
-                                                }
-                                            },
-                                            {
-                                                "key": "setText",
-                                                "val": {
-                                                    "jsdocType": "type",
-                                                    "jsType": "Function",
-                                                    "params": [
-                                                        {
-                                                            "jsdocType": "param",
-                                                            "name": "text",
-                                                            "description": "",
-                                                            "jsType": "String"
-                                                        }
-                                                    ],
-                                                    "returns": {
-                                                        "jsdocType": "returns",
-                                                        "description": "this",
-                                                        "jsType": "button"
-                                                    },
-                                                    "description": "Sets text to the button"
-                                                }
-                                            }
-                                        ]
-                                    },
-                                    "augments": {
-                                        "jsdocType": "type",
-                                        "jsType": "control"
-                                    },
-                                    "bem": {
-                                        "jsdocType": "bem",
-                                        "block": "button"
-                                    }
-                                },
-                                "classes": {
-                                    "button": {
+                                    "exports": {
                                         "jsdocType": "class",
                                         "name": "button",
                                         "proto": {
@@ -127,17 +76,73 @@ describe('Agrarium: Plugin BEM JSD', () => {
                                             "jsdocType": "bem",
                                             "block": "button"
                                         }
+                                    },
+                                    "classes": {
+                                        "button": {
+                                            "jsdocType": "class",
+                                            "name": "button",
+                                            "proto": {
+                                                "jsdocType": "type",
+                                                "jsType": "Object",
+                                                "props": [
+                                                    {
+                                                        "key": "getText",
+                                                        "val": {
+                                                            "jsdocType": "type",
+                                                            "jsType": "Function",
+                                                            "returns": {
+                                                                "jsdocType": "returns",
+                                                                "description": "",
+                                                                "jsType": "String"
+                                                            },
+                                                            "description": "Returns text of the button"
+                                                        }
+                                                    },
+                                                    {
+                                                        "key": "setText",
+                                                        "val": {
+                                                            "jsdocType": "type",
+                                                            "jsType": "Function",
+                                                            "params": [
+                                                                {
+                                                                    "jsdocType": "param",
+                                                                    "name": "text",
+                                                                    "description": "",
+                                                                    "jsType": "String"
+                                                                }
+                                                            ],
+                                                            "returns": {
+                                                                "jsdocType": "returns",
+                                                                "description": "this",
+                                                                "jsType": "button"
+                                                            },
+                                                            "description": "Sets text to the button"
+                                                        }
+                                                    }
+                                                ]
+                                            },
+                                            "augments": {
+                                                "jsdocType": "type",
+                                                "jsType": "control"
+                                            },
+                                            "bem": {
+                                                "jsdocType": "bem",
+                                                "block": "button"
+                                            }
+                                        }
                                     }
                                 }
-                            }
-                        ]
-                    }
-                }]
+                            ]
+                        }
+                    }]
+                }
             },
             {
-                bemjsd: [{
-                    jsd: {}
-                }]
+                data: {
+                    bemjsd: [{
+                        jsd: {}
+                    }]
+                }
             }
         ]);
     });
