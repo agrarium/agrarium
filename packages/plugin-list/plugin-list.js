6b5f034913b0b9128d882d4084ac69d7f65167ac
awinogradov <winogradovaa@gmail.com>
2018-03-29 01:12:30 +0300

    chore: rewrite PluginList to TS

--- packages/plugin-list/plugin-list.js
+++
@@ -1,9 +0,0 @@
-module.exports = class AgrariumList {
-    constructor(options = {}) {
-        this.hook = options.hook || (() => {});
-    }
-    
-    seed(chunk) {
-        this.hook(chunk);
-    }
-}
